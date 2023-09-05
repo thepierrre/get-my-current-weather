6732ad9489c6b1f624ca52985408c5c1230ee8cb
thepierrre <piotr.owczarczyk98@gmail.com>
2023-09-05 11:54:29 +0200

    cleaned the code

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,14 @@ const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const {
+  locationKey,
+  weatherAPIKey,
+  OpenWeatherAPIKey,
+} = require("./weather-app-credentials");
 
-const port = process.env.PORT;
+// const port = process.env.PORT;
+const port = 5014;
 
 app.use(express.json());
 
@@ -16,7 +22,8 @@ app.use(
 app.use("/location", async (req, res, next) => {
   const { lat, lon } = req.body;
 
-  const apiKey = process.env.LOCATIONKEY;
+  // const apiKey = process.env.LOCATIONKEY;
+  const APIKey = locationKey;
   const APIUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${apiKey}`;
 
   try {
@@ -30,18 +37,65 @@ app.use("/location", async (req, res, next) => {
   res.json(location);
 });
 
-app.use("/weather", async (req, res, next) => {
+// Get the coordinates based on the entered city.
+app.get("/coordinates", async (req, res, next) => {
   const { city } = req.query;
-  const apiKey = process.env.WEATHERKEY;
-  const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  const APIKey = OpenWeatherAPIKey;
+  const limit = 1;
+  const APIUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${APIKey}`;
 
   try {
     const response = await axios.get(APIUrl);
     const weather = response.data;
-
     res.json(weather);
-  } catch (error) {
+  } catch (err) {
     res.status(422).json({ errorMessage: "Couldn't find the entered city." });
+    console.log(err);
+  }
+});
+
+// Get weather and air quality based on the coordinates.
+app.get("/combined-data", async (req, res, next) => {
+  const { lat, lon } = req.query;
+  const APIKey = OpenWeatherAPIKey;
+
+  airQualityAPIUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+  weatherAPIURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+
+  try {
+    const [airQualityResponse, weatherResponse] = await Promise.all([
+      axios.get(airQualityAPIUrl),
+      axios.get(weatherAPIURL),
+    ]);
+    const airQualityData = airQualityResponse.data;
+    const weatherData = weatherResponse.data;
+
+    const combinedData = {
+      airQuality: airQualityData,
+      weather: weatherData,
+    };
+
+    res.json(combinedData);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Get the city name based on the coordinates.
+app.get("/city", async (req, res, next) => {
+  const { lat, lon } = req.query;
+
+  const APIKey = OpenWeatherAPIKey;
+  const limit = 1;
+  const APIUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${limit}&appid=${APIKey}`;
+
+  try {
+    const response = await axios.get(APIUrl);
+    const city = response.data;
+    res.json(city);
+  } catch (err) {
+    console.log(err);
   }
 });
 
